7c2a8589f1cd3ddcce42a5f60f566d841530469c
Irina Shyshkova <Irina_Shyshkova@epam.com>
2020-05-11 13:53:38 +0300

    feat: add env file

---
+++ build/precache-manifest.69a26707a9c30bd153136833ea874c6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8047ca41415a6014b666fd4e449ebda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14a3812782214bcfee35",
+    "url": "/static/js/2.2748a462.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2748a462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f4979c04ddeaeb104b",
+    "url": "/static/js/main.51c694ed.chunk.js"
+  },
+  {
+    "revision": "99a1df5724a42cc4fef3",
+    "url": "/static/js/runtime-main.4a3acf61.js"
+  }
+]);
--- src/components/Two.js
+++ src/components/Two.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 export default class Two extends React.Component {
+  
   render() {
+  
+    console.log(process.env.REACT_APP_CLIENT_ID); 
+    console.log(process.env.REACT_APP_KEY);
+    console.log(process.env.NODE_ENV);
+
     return <h2>Two</h2>;
   }    
 };
